d76a0c1df39e2563ce7ed3a46e425c155badb1f3
Catherine Loesch <catherine.loesch@protonmail.com>
2022-02-20 03:38:44 +0000

    refactoring

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,6 +1,5 @@
-
 class CashRegister
-    attr_accessor :total, :discount, :items, :transaction, :quantity
+    attr_accessor :total, :discount, :items, :prev_total, :prev_items
 
     def initialize(discount=0)
         @total = 0
@@ -9,23 +8,25 @@ class CashRegister
     end
 
     def add_item(title, price, quantity=1)
-        @quantity = quantity
-        @transaction = price * quantity
-        self.total += self.transaction
-        quantity.times {self.items << title}
-    end
+        @prev_items = self.items
+        quantity.times { self.items << title }
 
-    def apply_discount
-        if self.discount != 0
-            self.total -= (self.total * (self.discount.to_f/100))
-            "After the discount, the total comes to $#{self.total.to_i}."
-        else
-            "There is no discount to apply."
-        end
+        @prev_total = self.total
+        self.total += (price * quantity)
     end
 
     def void_last_transaction
-        self.total -= self.transaction
-        self.quantity.times {self.items.pop}
+        self.total = @prev_total
+        self.items = @prev_items
     end
+
+    def apply_discount
+        if discount == 0
+            "There is no discount to apply."
+        else
+            self.total -= discount.to_f/100 * self.total
+            "After the discount, the total comes to $#{self.total % 1 == 0 ? self.total = self.total.to_i : self.total}."
+        end
+    end
+
 end
